cd7d9869471f1a73dd420c8b31ca2b84ae59d035
daisy12321 <candydaisy12321@gmail.com>
2021-03-23 15:51:19 -0400

    Convert indentation to display better in github

--- training.py
+++ training.py
@@ -4,98 +4,98 @@ import pandas as pd
 from interpretableai import iai
 
 def get_X(df, drop_age, drop_eng):
-	# Only keep SMART features in X
-	cols_to_drop = [col for col in df.columns if 'smart' not in col]
-	X = df.drop(cols_to_drop, axis=1)
+  # Only keep SMART features in X
+  cols_to_drop = [col for col in df.columns if 'smart' not in col]
+  X = df.drop(cols_to_drop, axis=1)
 
-	# Remove SMART features correlated with age
-	if drop_age:
-		inds = [4, 9, 12, 192, 193, 240, 241, 242]
-		cols_age = [col for col in X.columns for i in inds
-								if ('_' + str(i) + '_') in col]
-		X = X.drop(cols_age, axis=1)
+  # Remove SMART features correlated with age
+  if drop_age:
+    inds = [4, 9, 12, 192, 193, 240, 241, 242]
+    cols_age = [col for col in X.columns for i in inds
+                if ('_' + str(i) + '_') in col]
+    X = X.drop(cols_age, axis=1)
 
-	# Remove engineered features
-	if drop_eng:
-		cols_engineered = [col for col in X.columns
-											 if ('raw_' in col) or ('normalized_' in col)]
-		X = X.drop(cols_engineered, axis=1)
+  # Remove engineered features
+  if drop_eng:
+    cols_engineered = [col for col in X.columns
+                       if ('raw_' in col) or ('normalized_' in col)]
+    X = X.drop(cols_engineered, axis=1)
 
-	# Return clean DataFrame
-	return(X)
+  # Return clean DataFrame
+  return(X)
 
 def train_OST(df, file_name, drop_age=True, drop_eng=True):
-	# Extract data fields to fit Optimal Survival Tree
-	X = get_X(df, drop_age=drop_age, drop_eng=drop_eng)
-	died = df['failed']
-	times = df['remaining_useful_life']
+  # Extract data fields to fit Optimal Survival Tree
+  X = get_X(df, drop_age=drop_age, drop_eng=drop_eng)
+  died = df['failed']
+  times = df['remaining_useful_life']
 
-	# Fit Optimal Survival Tree
-	grid = iai.GridSearch(
-						 iai.OptimalTreeSurvivor(
-							   random_seed=1,
-							   missingdatamode='separate_class',
-							   criterion='localfulllikelihood',
-							   minbucket=int(0.01 * len(X))
-						 ),
-						 max_depth=range(2, 5)
-         )
-	grid.fit(X, died.tolist(), times, validation_criterion='harrell_c_statistic')
+  # Fit Optimal Survival Tree
+  grid = iai.GridSearch(
+      iai.OptimalTreeSurvivor(
+          random_seed=1,
+          missingdatamode='separate_class',
+          criterion='localfulllikelihood',
+          minbucket=int(0.01 * len(X))
+      ),
+      max_depth=range(2, 5)
+  )
+  grid.fit(X, died.tolist(), times, validation_criterion='harrell_c_statistic')
 
-	# Write output models
-	grid.get_learner().write_html('models/' + file_name + '.html')
-	grid.write_json('models/' + file_name + '.json')
+  # Write output models
+  grid.get_learner().write_html('models/' + file_name + '.html')
+  grid.write_json('models/' + file_name + '.json')
 
 def train_OCT(df, file_name, drop_age=False, drop_eng=True):
-	# Separate machines which failed and never failed
-	machines_failures = list(df[df['failure'] == 1]['serial_number'].unique())
-	machines_no_failures = list(df['serial_number'].drop_duplicates() \
-                                	.replace(machines_failures, np.NaN).dropna())
+  # Separate machines which failed and never failed
+  machines_failures = list(df[df['failure'] == 1]['serial_number'].unique())
+  machines_no_failures = list(df['serial_number'].drop_duplicates() \
+                             .replace(machines_failures, np.NaN).dropna())
 
-	# Split machines between training and testing
-	np.random.seed(1)
-	machines_train = list(np.random.choice(machines_failures,
-																				 int(len(machines_failures) * 0.8),
-	                                       replace=False)) + \
-	                 list(np.random.choice(machines_no_failures,
-	                 											 int(len(machines_no_failures) * 0.8),
-	                                       replace=False))
-	machines_test = list(set(machines_failures + machines_no_failures).difference(
-											 set(machines_train)))
+  # Split machines between training and testing
+  np.random.seed(1)
+  machines_train = list(np.random.choice(machines_failures,
+                                         int(len(machines_failures) * 0.8),
+                                         replace=False)) + \
+                   list(np.random.choice(machines_no_failures,
+                                         int(len(machines_no_failures) * 0.8),
+                                         replace=False))
+  machines_test = list(set(machines_failures + machines_no_failures).difference(
+                       set(machines_train)))
 
-	# Set pair (machine, date) as multi-index
-	df = df.sort_values(['serial_number', 'datetime'])
-	df = df.set_index(['serial_number', 'datetime'])
+  # Set pair (machine, date) as multi-index
+  df = df.sort_values(['serial_number', 'datetime'])
+  df = df.set_index(['serial_number', 'datetime'])
 
-	# Split data between training and testing
-	df_train = df.loc[machines_train]
-	df_test = df.loc[machines_test]
+  # Split data between training and testing
+  df_train = df.loc[machines_train]
+  df_test = df.loc[machines_test]
 
-	# Extract data fields to fit Optimal Classification Tree
-	X_train = get_X(df_train, drop_age=drop_age, drop_eng=drop_eng)
-	X_test = get_X(df_test, drop_age=drop_age, drop_eng=drop_eng)
-	y_train = df_train['failure_next_30'].apply(
-								lambda x: 'Failure' if x else 'Operational')
-	y_test = df_test['failure_next_30'].apply(
-								lambda x: 'Failure' if x else 'Operational')
+  # Extract data fields to fit Optimal Classification Tree
+  X_train = get_X(df_train, drop_age=drop_age, drop_eng=drop_eng)
+  X_test = get_X(df_test, drop_age=drop_age, drop_eng=drop_eng)
+  y_train = df_train['failure_next_30'].apply(
+                lambda x: 'Failure' if x else 'Operational')
+  y_test = df_test['failure_next_30'].apply(
+                lambda x: 'Failure' if x else 'Operational')
 
-	print(X_train.shape)
-	print(X_test.shape)
-	# Fit Optimal Classification Tree
-	grid = iai.GridSearch(
-					   iai.OptimalTreeClassifier(
-					   	   random_seed=1,
-				         missingdatamode='separate_class',
-				         criterion='gini',
-				         minbucket=int(0.005 * len(X_train))
-				     ),
-		         max_depth=range(2, 5)
-		     )
-	grid.fit_cv(X_train, y_train)
+  print(X_train.shape)
+  print(X_test.shape)
+  # Fit Optimal Classification Tree
+  grid = iai.GridSearch(
+      iai.OptimalTreeClassifier(
+          random_seed=1,
+          missingdatamode='separate_class',
+          criterion='gini',
+          minbucket=int(0.005 * len(X_train))
+      ),
+      max_depth=range(2, 7)
+  )
+  grid.fit_cv(X_train, y_train)
 
-	# Write output models
-	grid.get_learner().write_html('models/' + file_name + '.html')
-	grid.write_json('models/' + file_name + '.json')
+  # Write output models
+  grid.get_learner().write_html('models/' + file_name + '.html')
+  grid.write_json('models/' + file_name + '.json')
 
 
 
@@ -149,5 +149,3 @@ df['datetime'] = pd.to_datetime(df['datetime'])
 
 # Train OST
 train_OST(df, '4_OST_Q1_20_no_age', drop_age=True, drop_eng=True)
-
-
